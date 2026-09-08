08db57682cc8302e55bfe43bfeba8f47e2351c53
Sylvain Riso <riso.sylvain@gmail.com>
2026-09-08 16:15:33 +0200

    feat: ajout du tableau de bord

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import categoriesRouter from "./routes/categories.js";
 import objetsRouter from "./routes/objets.js";
 import depotsRouter from "./routes/depots.js";
@@ -7,9 +8,9 @@ import statsRouter from "./routes/stats.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" with { type: "json"};
 
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/categories", categoriesRouter);
