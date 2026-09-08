daaf7a39f2dff18031cdc023567bcfd6c74e6d65
iveunem-bip <iveunem@gmail.com>
2026-09-08 18:22:52 +0200

    instalation de cors

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ import personnesRouter from "./routes/personnes.js";
 import statsRouter from "./routes/stats.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" with { type: "json"};
+import cors from "cors";
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/categories", categoriesRouter);
 app.use("/api/objets", objetsRouter);
