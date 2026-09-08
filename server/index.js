221b3fc7a56491fb0b41770d8b5150b3d224a830
radji-yanis <radjiyanis@gmail.com>
2026-09-08 11:17:07 +0200

    petite modif du back

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import categoriesRouter from "./routes/categories.js";
 import objetsRouter from "./routes/objets.js";
 import depotsRouter from "./routes/depots.js";
@@ -9,7 +10,7 @@ import swaggerDocument from "./swagger.json" with { type: "json"};
 
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/categories", categoriesRouter);
