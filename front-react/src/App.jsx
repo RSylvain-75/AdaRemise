9578c127f6295e41e06be464766bd64ed00bb377
Sylvain Riso <riso.sylvain@gmail.com>
2026-09-10 17:06:28 +0200

    fix: integre le dashboard avec le routeur

--- front-react/src/App.jsx
+++ front-react/src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import ListeObjets from "./components/ListeObjets/ListeObjets";
+import FicheObjet from "./components/FicheObjet/FicheObjet";
+import FormulaireObjet from "./components/FormulaireObjet/FormulaireObjet";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import "./App.css";
 
 function App() {
-  return <Dashboard/>;
+  return (
+    <Routes>
+      <Route path="/" element={<ListeObjets />} />
+      <Route path="/objets/nouveau" element={<FormulaireObjet />} />
+      <Route path="/objets/:id" element={<FicheObjet />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App;
