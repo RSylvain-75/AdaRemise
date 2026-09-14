4e92797fed98eefd5c3dac931655318006967270
iveunem-bip <iveunem@gmail.com>
2026-09-14 10:41:52 +0200

    mise a jour des routes

--- front-react/src/App.jsx
+++ front-react/src/App.jsx
@@ -3,6 +3,8 @@ import ListeObjets from "./components/ListeObjets/ListeObjets";
 import FicheObjet from "./components/FicheObjet/FicheObjet";
 import FormulaireObjet from "./components/FormulaireObjet/FormulaireObjet";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import Fiche_depot from "./components/Fiche_depot/Fiche_depot.jsx";
+import NouveauDepot from "./components/Nouveau_depot/Nouveau_depot.jsx";
 import "./App.css";
 
 function App() {
@@ -11,7 +13,8 @@ function App() {
       <Route path="/" element={<ListeObjets />} />
       <Route path="/objets/nouveau" element={<FormulaireObjet />} />
       <Route path="/objets/:id" element={<FicheObjet />} />
-
+      <Route path="/depot/:id" element={<Fiche_depot/>}/>
+      <Route path="/depots/nouveau" element={<NouveauDepot/>}/>
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
